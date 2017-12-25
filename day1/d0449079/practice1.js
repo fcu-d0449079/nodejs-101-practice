a2b4d0b634fb867cf27d6f42aea8c6aa522277ef
fcu-d0449079 <ink_zhuang@yahoo.com.tw>
2017-12-25 21:55:43 +0800

    Create practice1.js

---
+++ day1/d0449079/practice1.js
@@ -0,0 +1,23 @@
+{
+  "name": "d0449079",
+  "version": "1.0.0",
+  "description": "fcu",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "homework": "echo helloworld"
+  },
+  "author": "d0449079",
+  "license": "ISC",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/fcu-d0449079/"
+  },
+  "keywords": [],
+  "devDependencies": {
+    "mocha": "^4.0.1"
+  },
+  "dependencies": {
+    "axios": "^0.17.1"
+  }
+}
